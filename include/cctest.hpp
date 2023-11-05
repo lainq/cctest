a0d6443e24ba43e37c41365249f6163aefcb4ad9
lain <70764593+lainq@users.noreply.github.com>
2023-11-05 23:59:12 +0530

    fmt

--- include/cctest.hpp
+++ include/cctest.hpp
@@ -1,47 +1,45 @@
 #ifndef CCTEST_HPP_INCLUDED
 #define CCTEST_HPP_INCLUDED
 
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <memory>
-#include <functional>
-#include <exception>
-#include <sstream>
-#include <iostream>
-#include <optional>
 
 #ifndef __FUNCTION_NAME__
-  #ifdef WIN32 
-    #define __FUNCTION_NAME__ __FUNCTION__
-  #else
-    #define __FUNCTION_NAME__ __func__
-  #endif
+#ifdef WIN32
+#define __FUNCTION_NAME__ __FUNCTION__
+#else
+#define __FUNCTION_NAME__ __func__
+#endif
 #endif
 
 #define TEST_CASE(name) static void test_##name()
 
 #define RUN_TESTS() test_collection::run_tests()
-#define REGISTER_TEST(name) \
-test_collection::append(#name, \
-std::function<std::remove_pointer_t<decltype(test_##name)\
->>(test_##name))
+#define REGISTER_TEST(name)                                               \
+  test_collection::append(                                                \
+      #name, std::function<std::remove_pointer_t<decltype(test_##name)>>( \
+                 test_##name))
 
-#define ASSERT(expression) assertions::assert(#expression, \
-  __FUNCTION_NAME__, __FILE__, expression)
+#define ASSERT(expression) \
+  assertions::assert(#expression, __FUNCTION_NAME__, __FILE__, expression)
 
 #define ASSERT_EQ(expr1, expr2) ASSERT(expr1 == expr2)
 #define ASSERT_NEQ(expr1, expr2) ASSERT(expr1 != expr2)
 
-class assertion_failure: public std::exception {
-private:
+class assertion_failure : public std::exception {
+ private:
   std::string expression, func_name, fname;
-public:
+
+ public:
   assertion_failure(const std::string&& expression_,
-    const std::string&& func_name_,
-    const std::string&& fname_):
-    expression(expression_),
-  func_name(func_name_),
-  fname(fname_) {}
+                    const std::string&& func_name_, const std::string&& fname_)
+      : expression(expression_), func_name(func_name_), fname(fname_) {}
 
   std::string to_string() const {
     std::stringstream error_stream;
@@ -52,20 +50,23 @@ public:
   }
 };
 
-class unit_test {public:
+class unit_test {
+ public:
   using test_function_t = std::function<void()>;
-  unit_test(const std::string&& tname, test_function_t&& func) :
-    name(std::move(tname)), test_func(std::move(func)) {}
+  unit_test(const std::string&& tname, test_function_t&& func)
+      : name(std::move(tname)), test_func(std::move(func)) {}
   std::optional<assertion_failure> run() const {
     try {
-      test_func();    } catch(assertion_failure& exception) {
-           return exception;
+      test_func();
+    } catch (assertion_failure& exception) {
+      return exception;
     }
     return {};
   }
-  const std::string& get_name() const { return name ;}
+  const std::string& get_name() const { return name; }
 
-private:  const std::string name;
+ private:
+  const std::string name;
   unit_test::test_function_t test_func;
 };
 
@@ -73,13 +74,16 @@ constexpr const char* failed_str = "\033[31mFAILED\033[39m";
 constexpr const char* passed_str = "\033[32mPASSED\033[39m";
 
 class test_collection {
-private:
+ private:
   test_collection() = delete;
-  inline static std::vector<std::unique_ptr<unit_test>> tests {};
-public:
-  template<typename... Args>
-  static void append(const std::string&& expression, unit_test::test_function_t&& func) {
-    std::unique_ptr<unit_test> test { new unit_test(std::move(expression), std::move(func)) };
+  inline static std::vector<std::unique_ptr<unit_test>> tests{};
+
+ public:
+  template <typename... Args>
+  static void append(const std::string&& expression,
+                     unit_test::test_function_t&& func) {
+    std::unique_ptr<unit_test> test{
+        new unit_test(std::move(expression), std::move(func))};
     tests.push_back(std::move(test));
   }
 
@@ -87,38 +91,44 @@ public:
     std::ios_base::sync_with_stdio(false);
     std::stringstream out_stream;
     std::vector<std::string> failures;
-    out_stream << "Running " << tests.size() << (tests.size() == 1 ? " test" : " tests");
+    out_stream << "Running " << tests.size()
+               << (tests.size() == 1 ? " test" : " tests");
 
-    for(const auto& unit_test: tests) {
+    for (const auto& unit_test : tests) {
       auto result = unit_test->run();
-      out_stream << "\ntest " << unit_test->get_name() << " ... " <<
-        (result.has_value() ?  failed_str : passed_str);
-      if(result.has_value()) {
+      out_stream << "\ntest " << unit_test->get_name() << " ... "
+                 << (result.has_value() ? failed_str : passed_str);
+      if (result.has_value()) {
         const auto failure = result.value();
         failures.push_back(std::move(failure.to_string()));
       }
     }
-    if(failures.size() > 0) {
-    out_stream << "\nfailures:\n";
-    for(const std::string& failure: failures) {
-      out_stream << failure << "\n";
-    }} else { out_stream << "\n";}
+    if (failures.size() > 0) {
+      out_stream << "\nfailures:\n";
+      for (const std::string& failure : failures) {
+        out_stream << failure << "\n";
+      }
+    } else {
+      out_stream << "\n";
+    }
 
     const std::size_t passed_count = tests.size() - failures.size();
-    out_stream << "test result:" << (failures.size() > 0 ? 
-      failed_str: passed_str) << ". " << passed_count << " passed; " <<
-      failures.size() << " failed;\n";
+    out_stream << "test result:"
+               << (failures.size() > 0 ? failed_str : passed_str) << ". "
+               << passed_count << " passed; " << failures.size()
+               << " failed;\n";
     std::cout << out_stream.rdbuf();
   }
 };
 
 class assertions {
-public:
+ public:
   static void assert(const std::string&& expression_str,
-    const std::string&& func_name,
-    const std::string&& fname, bool expression) {
-    if(!expression) {      throw assertion_failure(std::move(expression_str),
-       std::move(func_name), std::move(fname));
+                     const std::string&& func_name, const std::string&& fname,
+                     bool expression) {
+    if (!expression) {
+      throw assertion_failure(std::move(expression_str), std::move(func_name),
+                              std::move(fname));
     }
   }
 };
